24a29adf7082da458855b981371b3b3070ee7c58
amira3a <amira3a2780@yahoo.com>
2025-04-13 20:46:03 +0200

    Security development

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9XJyDspkltGMVjgJ5wsCcZFz3DSzIZek",
-  authDomain: "store-dashboard-ce29c.firebaseapp.com",
-  projectId: "store-dashboard-ce29c",
-  storageBucket: "store-dashboard-ce29c.firebasestorage.app",
-  messagingSenderId: "353415080579",
-  appId: "1:353415080579:web:5acf53e02be5aaa85149b6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
